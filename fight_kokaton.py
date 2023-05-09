bfa0284edcfe37bfd372d8e600916ad04c26cf84
tomoki <c0a2205445@edu.teu.ac.jp>
2023-05-09 16:38:47 +0900

    switch

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -177,7 +177,7 @@ def main():
 
         
 
-        if beam is not None:  #  ビームが存在しているとき
+        if beam is not None:  #  ビームが存在しているとき 
             beam.update(screen)
             if bomb is not None and beam   ._rct.colliderect(bomb._rct):
                 beam = None
